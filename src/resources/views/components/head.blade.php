f7162881987e4a232e1b07bd4d74b6763d3fa285
uenoarisa <arisa-choco0326@docomo.ne.jp>
2023-06-20 16:15:20 +0900

    トップページのview表示できた

---
+++ src/resources/views/components/head.blade.php
@@ -0,0 +1,15 @@
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>POSSE 初めてのWeb制作</title>
+  <!-- スタイルシート読み込み -->
+  <link rel="stylesheet" href="./assets/styles/common.css">
+  <!-- Google Fonts読み込み -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Plus+Jakarta+Sans:wght@400;700&display=swap"
+    rel="stylesheet">
+  <script src="../assets/scripts/common.js" defer></script>
+</head>
